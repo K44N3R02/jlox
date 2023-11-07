578a8ac3155ca56ccb71868e9399c39fae783613
k44n <kaanemireroz@gmail.com>
2023-11-07 22:22:23 +0300

    Build maven project

---
+++ demo/src/main/java/com/jlox/Main.java
@@ -0,0 +1,11 @@
+package demo.src.main.java.com.jlox;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ demo/src/test/java/com/jlox/AppTest.java
@@ -0,0 +1,22 @@
+package demo.src.test.java.com.jlox;
+
+/*
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ * /
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     * /
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
+*/
